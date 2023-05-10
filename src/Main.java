e3b85595a82a68adbe8e38ce70de4b459797522f
conditional <105pkn@mail.ru>
2023-05-10 22:13:25 +0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+// задача 1
+        int age = 17;
+        if (age >= 18) {
+            System.out.printf("Если возраст человека равен " + age + ", то он совершеннолетний");
+            } else {
+            System.out.printf("Если возраст человека равен " + age
+                    + ", то он не достиг совершеннолетия, нужно немного подождать");
+        }
+// задача 2
+                
+
+    }
+}
