13b7353e59d11782735a38ac751c9497d0e9e714
conditional <105pkn@mail.ru>
2023-05-11 00:15:41 +0700

    second commit

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,24 @@ public class Main {
 // задача 1
         int age = 50;
         if (age >= 18) {
-            System.out.printf("Если возраст человека равен " + age + ", то он совершеннолетний");
+            System.out.println("Если возраст человека равен " + age + ", то он совершеннолетний.");
             } else {
-            System.out.printf("Если возраст человека равен " + age
-                    + ", то он не достиг совершеннолетия, нужно немного подождать");
+            System.out.println("Если возраст человека равен " + age
+                    + ", то он не достиг совершеннолетия, нужно немного подождать.");
         }
 // задача 2
-                
-
+        int temperature = 10;
+        if (temperature >= 5) {
+            System.out.println("На улице " + temperature + " градусов, можно идти без шапки.");
+        } else {
+            System.out.println("На улице " + temperature + " градусов, нужно надеть шапку.");
+        }
+// задача 3
+        int speed =51;
+        if (speed > 60) {
+            System.out.println("Если скорость " + speed + ", то придется заплатить штраф");
+        } else {
+            System.out.println("Если скорость " + speed + ", то можно ездить спокойно");
+        }
     }
 }
