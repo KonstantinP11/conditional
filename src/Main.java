4ad97fc2672b27896cf08ab5610826d5b3136d5b
conditional <105pkn@mail.ru>
2023-05-11 23:49:26 +0700

    домашка готова

--- src/Main.java
+++ src/Main.java
@@ -23,34 +23,27 @@ public class Main {
             System.out.println("Если скорость " + speed + ", то можно ездить спокойно");
         }
 // задача 4
-        age = 10;
+        age = 1;
         if (age < 2) {
             System.out.println("Если возраст человека равен " + age + ", то ему пора спать.");
-        }
-        if (age > 2 && age <= 6) {
+        } else if (age > 2 && age <= 6) {
             System.out.println("Если возраст человека равен " + age + ", то ему нужно ходить в детский сад");
-        }
-        if (age >= 7 && age <= 18) {
+        } else if (age >= 7 && age <= 18) {
             System.out.println("Если возраст человека равен " + age + ", то ему нужно ходить в школу");
-        }
-        if (age > 18 && age <= 24) {
+        } else if (age > 18 && age <= 24) {
             System.out.println("Если возраст человека равен " + age + ", то ему нужно ходить в университет");
-        }
-        if (age > 24 && age <= 60) {
+        } else if (age > 24 && age <= 60) {
             System.out.println("Если возраст человека равен " + age + ", то ему нужно ходить на работу");
-        }
-        if (age > 60) {
+        } else {
             System.out.println("Если возраст человека равен " + age + ", то ему можно отдохнуть");
         }
 // задача 5
-        age = 3;
+        age = 33;
         if (age < 5) {
             System.out.println("Если возраст ребенка  равен " + age + ", то он не может кататься на аттракционе.");
-        }
-        if (age >= 5 && age <= 14) {
+        } else if (age >= 5 && age <= 14) {
             System.out.println("Если возраст ребенка  равен " + age + ", то он может кататься только в сопровождении взрослого.");
-        }
-        if (age > 14) {
+        } else {
             System.out.println("Если возраст ребенка  равен " + age + ", то он может кататься без сопровождения взрослого.");
         }
 // задача 6
@@ -70,14 +63,26 @@ public class Main {
         }
 
 // задача 7
-        int one;
-        int two;
-        int three;
+        int one = 1;
+        int two = 10;
+        int three = 100;
+        if (one >= two) {
+            if (one > three) {
+                System.out.println("Большее число one = " + one);
+            } else {
+                System.out.println("Большее число three = " + three);
+            }
+        } else {
+            if (two > three) {
+                System.out.println("Большее число two = " + two);
+            } else {
+                System.out.println("Большее число three = " + three);
 
+            }
+        }
 
 
     }
-
 
 
 }
